976f8efe2145f176e5d15e938ce268de29786875
Tamar Rosental <tamarr@nayax.com>
2023-08-22 10:08:03 +0300

    code first create class

---
+++ ToDoList.Db/Table/Task.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Db.Table
+{
+    public class Task
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TaskId { get; set; }
+        public int? ParentTaskId { get; set; }
+
+        [StringLength(80, MinimumLength = 1)]
+        public string Title { get; set; }
+        [StringLength(500, MinimumLength = 1)]
+        public string Description { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public int Status { get; set; }
+
+
+
+        [DefaultValue(false)]
+        public bool IsDone { get; set; }
+    }
+}
---
+++ ToDoList.Db/Table/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoList.Db.Table
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+
+        [StringLength(80, MinimumLength = 1)]
+        public string? Name { get; set; }
+        public DateTime CreateDate { get; set; }
+        [DefaultValue(true)]
+        public bool IsActive { get; set; }
+    }
+}
