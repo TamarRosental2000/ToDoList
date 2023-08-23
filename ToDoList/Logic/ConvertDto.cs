2ddf91fa8311b8252fd3b0a77c631bd50874b7c1
Tamar Rosental <tamarr@nayax.com>
2023-08-23 11:18:31 +0300

    convert dto

---
+++ ToDoList/Logic/ConvertDto.cs
@@ -0,0 +1,23 @@
+﻿using ToDoList.Db.Table;
+
+namespace ToDoList.Api.Logic
+{
+    public class ConvertDto
+    {
+        public TaskItem ConvertToDto(TaskItemModel taskItemModel)
+        {
+            return new TaskItem
+            {
+                CreateDate= taskItemModel.CreateDate,
+                Description= taskItemModel.Description,
+                DueDate= taskItemModel.DueDate,
+                IsDone= taskItemModel.IsDone,
+                ParentTaskId= taskItemModel.ParentTaskId,   
+                Status= taskItemModel.Status,
+                TaskId= taskItemModel.TaskId,
+                Title= taskItemModel.Title,
+                UserId = taskItemModel.UserId
+            };
+        }
+    }
+}
